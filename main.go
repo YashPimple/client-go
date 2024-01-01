a4b4291deaa913560e82dc3c6ce1d1e7f77efb93
Yash Pimple <97302447+YashPimple@users.noreply.github.com>
2024-01-01 17:06:17 +0530

    Update main.go

--- main.go
+++ main.go
@@ -11,34 +11,48 @@ import (
 )
 
 func main() {
-	kubeconfig := flag.String("kubeconfig", "/Users/yashnileshpimple/.kube/config", "Location to config file")
+	kubeconfig := flag.String("kubeconfig", "/.kube/config", "Location to our config file")
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
-		//handle err
+		fmt.Println("Unable to find kubeconfig file")
 	}
 
+	var namespace string
+	fmt.Println("Enter the Namespace to get resources")
+	fmt.Scan(&namespace)
+	// use to interact with diff resources present in k8 it like an set of clients
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		//handle err
+		fmt.Println("clienset not running")
 	}
-	//pods are from corev1 resources()
+
 	ctx := context.Background()
-	pods, err := clientset.CoreV1().Pods("keptn-lifecycle-toolkit-system").List(ctx, metav1.ListOptions{})
+	pods, err := clientset.CoreV1().Pods(namespace).List(ctx, metav1.ListOptions{})
 	if err != nil {
-		//handle err
+		fmt.Println("Unable to find resources pod")
 	}
-	fmt.Println("Pods from keptn-lifecycle-toolkit-system namespace are")
+	fmt.Println("Pods in following namespace are:")
 	for _, pod := range pods.Items {
-		fmt.Printf("%s", pod.Name)
+		fmt.Printf("%s\n", pod.Name)
 	}
 
-	deployments, err := clientset.AppsV1().Deployments("keptn-lifecycle-toolkit-system").List(ctx, metav1.ListOptions{})
+	deployments, err := clientset.AppsV1().Deployments(namespace).List(ctx, metav1.ListOptions{})
 	if err != nil {
-		//handle funct
+		fmt.Println("Unable to find resource deployment")
 	}
-	fmt.Println("Deployments from keptn-lifecycle-toolkit-system namespace are")
+	fmt.Println("Deployments in following namespace are:")
 	for _, deployment := range deployments.Items {
-		fmt.Printf("%s", deployment.Name)
+		fmt.Printf("%s\n", deployment.Name)
 	}
 
+	services, err := clientset.CoreV1().Services(namespace).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		fmt.Println("Unable to find resource service")
+	}
+	fmt.Println("Service in following namespace are:")
+	for _, service := range services.Items {
+		fmt.Printf("%s\n", service.Name)
+	}
+	// fmt.Println(clientset)
+	// fmt.Println(config)
 }
