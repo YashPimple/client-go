d0b2817f530fbb84d9af6765fc80ab9cf2e6c053
Yash Pimple <yashpimple22@gmail.com>
2024-01-02 14:57:14 +0530

    Informer and cache
    Signed-off-by: Yash Pimple <yashpimple22@gmail.com>

--- main.go
+++ main.go
@@ -4,19 +4,22 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	kubeconfig := flag.String("kubeconfig", ".kube/config", "Location to our config file")
+	kubeconfig := flag.String("kubeconfig", "/Users/yashnileshpimple/.kube/config", "Location to our config file")
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
 		fmt.Printf("error %s building kubeconfig file", err.Error())
-		config, err := rest.InClusterConfig()
+		//config, err := rest.InClusterConfig()
 		if err != nil {
 			fmt.Print("Error %s getting incluster config", err.Error())
 		}
@@ -30,6 +33,34 @@ func main() {
 	if err != nil {
 		fmt.Printf("Error %s creating clienset", err.Error())
 	}
+
+	// these code provide us info from all shared namespace
+	informerfactory := informers.NewSharedInformerFactory(clientset, 30*time.Second)
+	//these code will provide us only info of selected namespace
+	informers.NewFilteredSharedInformerFactory(clientset, 10*time.Second, namespace, func(lo *metav1.ListOptions) {
+		lo.LabelSelector = ""
+		lo.APIVersion = ""
+	})
+
+	podinformer := informerfactory.Core().V1().Pods()
+	podinformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(new interface{}) {
+			fmt.Println("add was called")
+		},
+		UpdateFunc: func(old, new interface{}) {
+			fmt.Println("updated was called")
+		},
+		DeleteFunc: func(obj interface{}) {
+			fmt.Println("delete was called")
+		},
+	})
+	informerfactory.Start(wait.NeverStop)            //start
+	informerfactory.WaitForCacheSync(wait.NeverStop) //waits for cache
+	pod, err := podinformer.Lister().Pods(namespace).Get(namespace)
+	if err != nil {
+		fmt.Println("Unable to fetch resources")
+	}
+	fmt.Println(pod)
 
 	ctx := context.Background()
 	pods, err := clientset.CoreV1().Pods(namespace).List(ctx, metav1.ListOptions{})
